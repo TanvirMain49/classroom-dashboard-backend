3ef08400406c5623c5b8c93d1d8387d8075d883d
TanvirMain49 <tanvirmahin607@gmail.com>
2026-02-11 23:35:06 +0600

    added all the possible tables in the nenon db

--- src/schema/app.ts
+++ src/schema/app.ts
@@ -1,10 +1,16 @@
 import { relations } from "drizzle-orm";
-import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp, text, pgEnum, jsonb, index, uniqueIndex } from "drizzle-orm/pg-core";
+import { user } from "./auth";
 
 const timestamps = {
     createdAt : timestamp('created_at').defaultNow().notNull(),
     updatedAt : timestamp('updated_at').defaultNow().$onUpdate(()=> new Date()).notNull()
 }
+export const classStatusEnum = pgEnum("class_status", [
+  "active",
+  "inactive",
+  "archived",
+]);
 
 export const departments = pgTable('departments', {
     id : integer('id').primaryKey().generatedAlwaysAsIdentity(),
@@ -12,7 +18,7 @@ export const departments = pgTable('departments', {
     name : varchar('name', { length: 255 }).notNull(),
     description : varchar('description', { length: 255 }),
     ...timestamps,
-})
+});
 
 export const subjects = pgTable('subjects', {
     id : integer('id').primaryKey().generatedAlwaysAsIdentity(),
@@ -21,16 +27,85 @@ export const subjects = pgTable('subjects', {
     name : varchar('name', { length: 255 }).notNull(),
     description : varchar('description', { length: 255 }),
     ...timestamps,
-})
+});
 
-export const departmentRelations = relations(departments, ({many})=> ({ subjects: many(subjects) }))
+export const classes = pgTable("classes", {
+    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+    subjectId: integer('subject_id').references(()=> subjects.id, { onDelete: "cascade" }),
+    teacherId: text('teacher_id').references(()=> user.id, { onDelete: "cascade" }),
+    inviteCode: varchar('invite_code', { length: 50 }).notNull().unique(),
+    name: varchar('name', { length: 255 }).notNull(),
+    bannerCldPubId: text('banner_cld_pub_id'),
+    bannerUrl: text('banner_url'),
+    capacity: integer("capacity").notNull().default(50),
+    description: text("description"),
+    status: classStatusEnum("status").notNull().default("active"),
+    schedules: jsonb("schedules").notNull(),
+    
+    ...timestamps
+}, 
+(table)=>({
+        subjectIdIdx: index("classes_subject_id_idx").on(table.subjectId),
+        teacherIdIdx: index("classes_teacher_id_idx").on(table.teacherId),
+    })
+);
 
-export const subjectsRelations = relations(subjects, ({ one })=> ({
+export const enrollments = pgTable("enrollments", {
+    id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+    studentId: text("student_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+    classId: integer("class_id")
+      .notNull()
+      .references(() => classes.id, { onDelete: "cascade" }),
+
+    ...timestamps,
+  },
+  (table) => ({
+    studentIdIdx: index("enrollments_student_id_idx").on(table.studentId),
+    classIdIdx: index("enrollments_class_id_idx").on(table.classId),
+    studentClassUnique: uniqueIndex("enrollments_student_class_unique").on(
+      table.studentId,
+      table.classId
+    ),
+  })
+);
+
+
+export const departmentRelations = relations(departments, ({many})=> ({ 
+    subjects: many(subjects) 
+}))
+
+export const subjectsRelations = relations(subjects, ({ one, many })=> ({
     department : one(departments, {
         fields : [subjects.departmentId],
         references : [departments.id],
-    })
+    }),
+    classes: many(classes)
 }));
+
+export const classesRelations = relations(classes, ({ one, many })=>({
+    subject: one(subjects, {
+        fields: [classes.subjectId],
+        references: [subjects.id]
+    }),
+    teacher: one(user, {
+        fields: [classes.teacherId],
+        references: [user.id]
+    }),
+    enrollments: many(enrollments),
+}));
+
+export const enrollmentsRelations = relations(enrollments, ({ one })=>({
+    student: one(user, {
+        fields: [enrollments.studentId],
+        references: [user.id]
+    }),
+    class: one(classes, {
+        fields: [enrollments.classId],
+        references: [classes.id]
+    })
+}))
 
 
 export type Department = typeof departments.$inferSelect;
@@ -38,3 +113,9 @@ export type NewDepartment = typeof departments.$inferInsert;
 
 export type Subjects = typeof subjects.$inferSelect;
 export type NewSubjects = typeof subjects.$inferInsert;
+
+export type Classes = typeof classes.$inferSelect;
+export type NewClasses = typeof classes.$inferInsert;
+
+export type Enrollment = typeof enrollments.$inferSelect;
+export type NewEnrollment = typeof enrollments.$inferInsert;
---
+++ type.d.ts
@@ -0,0 +1,9 @@
+export type Schedule = {
+  day: string;
+  startTime: string;
+  endTime: string;
+};
+
+type UserRoles = "admin" | "teacher" | "student";
+
+type RateLimitRole = UserRoles | "guest";
